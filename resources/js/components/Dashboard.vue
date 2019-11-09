0b5c9a73c77cd3b39cf27930aee55a42778d7d4c
Pham Dang Thang <phamdangthang98@gmail.com>
2019-11-09 22:34:33 +0700

    update

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <section class="content-header">
+            <h1>Dashboard</h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
+        </section>
+        <section class="content">
+            <h5>I'm dashboard component</h5>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+
+</script>
