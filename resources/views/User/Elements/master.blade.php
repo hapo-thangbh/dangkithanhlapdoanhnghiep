fe45f4f6b6ffe8193d6d53aaca1375220b0e4d7a
Pham Dang Thang <phamdangthang98@gmail.com>
2019-11-20 20:24:52 +0700

    update

--- resources/views/User/Elements/master.blade.php
+++ resources/views/User/Elements/master.blade.php
@@ -5,10 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Zing</title>
-	{{-- <link rel="stylesheet" href="{{ asset('css/frontend/reset.css') }}" type="text/css"> --}}
-	<link rel="stylesheet" href="{{ asset('css/frontend/app.css') }}" type="text/css">
+	<link rel="stylesheet" href="{{ asset('css/frontend/reset.css') }}" type="text/css">
 	<link rel="stylesheet" href="{{ asset('css/frontend/style.css') }}" type="text/css">
-	{{-- <link rel="stylesheet" href="{{ asset('css/frontend/media.css') }}" type="text/css"> --}}
     <link rel="stylesheet" href="{{ asset('js/frontend/slick/slick.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/frontend/font-awesome.css') }}" type="text/css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
