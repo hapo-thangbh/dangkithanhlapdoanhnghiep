3e7a2b0d74c7c65618a3f99cff97fb5a38ab7ee7
Dang Thang <phamdangthang98@gmail.com>
2019-11-28 14:55:45 +0700

    'update'

--- database/migrations/2019_11_20_135939_create_posts_table.php
+++ database/migrations/2019_11_20_135939_create_posts_table.php
@@ -17,7 +17,7 @@ class CreatePostsTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('description');
-            $table->boolean('status')->default(0);
+            $table->boolean('status')->default(0)->nullable();
             $table->integer('view')->default(0);
             $table->timestamps();
         });
