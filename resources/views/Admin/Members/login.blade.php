1be6941882a2bf4f1fccbe05a0a63b779c82eaa4
Dang Thang <phamdangthang98@gmail.com>
2020-01-06 09:51:21 +0700

    update

--- resources/views/Admin/Members/login.blade.php
+++ resources/views/Admin/Members/login.blade.php
@@ -44,7 +44,7 @@
                 <div class="row">
                     <div class="col-xs-8">
                         <label class="checkbox-primary">
-                            <input type="checkbox" id="check" name="remember">
+                            <input type="checkbox" id="check" name="remember" checked>
                             <span></span>
                         </label>
                         <label class="lbl-checkbox-primary" for="check">Nhớ mật khẩu</label>
